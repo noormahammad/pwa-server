756bca3473900ea4aae174e6abe6cdc7f4f77753
trangnt58 <trang2uet@gmail.com>
2017-02-07 22:17:39 +0700

    update CRUD

--- app.js
+++ app.js
@@ -7,11 +7,20 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var words = require('./routes/words');
+
+var Word = require('./models/word.model');
 
 var app = express();
-var mongoose   = require('mongoose');
+var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://trang2uet:pwa@ds049631.mlab.com:49631/pwa'); // connect to our databas
+var db = 'mongodb://trang2uet:pwa@ds049631.mlab.com:49631/pwa';
+
+mongoose.connect(db, function (error) {
+    if (error) {
+        console.log(error);
+    }
+}); // connect to our databas
 
 
 // view engine setup
@@ -29,6 +38,79 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/words', words);
+
+app.post('/test', function (req, res) {
+  res.send('Got a POST request')
+})
+
+
+// Get all words
+app.get('/api/words', function(req, res) {
+	console.log('searching...');
+    // use mongoose to get all words in the database
+    Word.find(function(err, words) {
+        // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+        if (err)
+            res.send(err)
+        res.json(words); // return all reviews in JSON format
+    });
+});
+
+app.get('/api/word/:id', function(req, res) {
+	Word.findOne({
+		_id: req.params.id
+	})
+	.exec(function(err, word) {
+		if(err) {
+			res.send('error occured');
+		} else {
+			console.log(word);
+			res.json(word);
+		}
+	})
+});
+
+app.post('/api/word', function(req, res) {
+	Word.create(req.body, function(err, word) {
+		if (err) {
+			res.send('error saving word');
+		} else {
+			console.log(word);
+			res.send(word);
+		}
+	})
+});
+
+app.put('/api/word/:id', function(req, res) {
+	Word.findOneAndUpdate({
+		_id: req.params.id
+	}, 
+	{ $set: { content: req.body.content}},
+	{ upsert: true },
+	function (err, newWord) {
+		if(err) {
+			console.log('error occured');
+		} else {
+			console.log(newWord);
+			res.send(newWord);
+		}
+	});
+});
+
+
+app.delete('/api/word/:id', function(req, res) {
+	Word.findOneAndRemove({
+		_id: req.params.id
+	}, function(err, word){
+		if(err) {
+			res.send('error deleting');
+		} else {
+			console.log(word);
+			res.status(204);
+		}
+	});
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -47,6 +129,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error.html');
 });
+
 
 
 module.exports = app;
---
+++ models/word.model.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var WordSchema = new Schema({
+  id: String,
+  content: String,
+  meaning: String,
+  sound_us: String,
+  sound_uk: String
+}, {collection: 'words'});
+
+module.exports = mongoose.model('Word', WordSchema);
